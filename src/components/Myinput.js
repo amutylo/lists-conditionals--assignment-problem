230e53149e43a229fd52385e02a62be7e72cf274
andrii mutylo <amutylo@gmail.com>
2018-06-01 17:31:39 +0300

    update Myinput component

--- src/components/Myinput.js
+++ src/components/Myinput.js
@@ -4,15 +4,21 @@ export default class Myinput extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      wordlenth: 0
+      wordlength: 0
     }
   }
   
+  countWords(text) {
+      this.setState(
+        {wordlength: text.length}
+      )
+  } 
+
   render() {
     return (
       <div>
-        <input name="myinput" type="text" />
-        <p>The length of the word entered is: { this.state.wordlenth } chars</p>
+        <input name="myinput" type="text" onChange={(e) => { this.countWords(e.target.value) }} />
+        <p>The length of the word entered is: { this.state.wordlength } chars</p>
       </div>
     );
   }
